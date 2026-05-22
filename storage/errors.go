6a9a416c0356207c9a64db7539af6086b4b0d614
morehao <morehao@qq.com>
2026-05-22 11:27:33 +0800

    feat(storage): define new error model

--- storage/errors.go
+++ storage/errors.go
@@ -4,6 +4,7 @@ import "github.com/morehao/golib/storage/internal/core"
 
 var (
 	ErrInvalidConfig  = core.ErrInvalidConfig
-	ErrObjectNotFound = core.ErrObjectNotFound
 	ErrInvalidKey     = core.ErrInvalidKey
+	ErrObjectNotFound = core.ErrObjectNotFound
+	ErrNotSupported   = core.ErrNotSupported
 )
--- storage/internal/core/errors.go
+++ storage/internal/core/errors.go
@@ -3,7 +3,8 @@ package core
 import "errors"
 
 var (
-	ErrInvalidConfig  = errors.New("invalid storage config")
-	ErrObjectNotFound = errors.New("storage object not found")
-	ErrInvalidKey     = errors.New("invalid object key")
+	ErrInvalidConfig  = errors.New("storage: invalid config")
+	ErrInvalidKey     = errors.New("storage: invalid key")
+	ErrObjectNotFound = errors.New("storage: object not found")
+	ErrNotSupported   = errors.New("storage: operation not supported")
 )
