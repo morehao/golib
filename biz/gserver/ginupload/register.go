3427282f15a33f80397b25448032ea2d74e53242
morehao <morehao@qq.com>
2026-05-28 20:24:30 +0800

    feat(ginupload): add Register function

---
+++ biz/gserver/ginupload/register.go
@@ -0,0 +1,23 @@
+package ginupload
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/morehao/golib/filestore"
+)
+
+const defaultFilePrefix = "/file"
+
+func Register(group *gin.RouterGroup, fs *filestore.FileStore) {
+	r := group.Group(defaultFilePrefix)
+	{
+		r.POST("/upload", handleUpload(fs))
+		r.POST("/checkExist", handleCheckExist(fs))
+		r.POST("/initMultipartUpload", handleInitMultipartUpload(fs))
+		r.POST("/presignUploadPartURL", handlePresignUploadPartURL(fs))
+		r.POST("/completeMultipartUpload", handleCompleteMultipartUpload(fs))
+		r.POST("/abortMultipartUpload", handleAbortMultipartUpload(fs))
+		r.POST("/getFileDetail", handleGetFileDetail(fs))
+		r.POST("/presignGetFileURL", handlePresignGetFileURL(fs))
+		r.POST("/deleteFile", handleDeleteFile(fs))
+	}
+}
