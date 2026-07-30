aa42afe28508413061a23a0f4cdf95d417c589d4
morehao <morehao@qq.com>
2026-07-30 18:42:27 +0800

    refactor(ginserver): move API version constants from gconstant to ginserver

---
+++ biz/gserver/ginserver/const.go
@@ -0,0 +1,9 @@
+package ginserver
+
+const (
+	ApiVersionV1 = "v1"
+	ApiVersionV2 = "v2"
+	ApiVersionV3 = "v3"
+	ApiVersionV4 = "v4"
+	ApiVersionV5 = "v5"
+)
--- gconstant/api.go
+++
@@ -1,6 +0,0 @@
-package gconstant
-
-const (
-	ApiVersionV1 = "v1"
-	ApiVersionV2 = "v2"
-)
