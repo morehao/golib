2da8317320a0458ae22f5b856d3608c7c022e9eb
morehao <morehao@qq.com>
2026-07-31 14:27:37 +0800

    refactor(glog): rename OptConfig to LoggerOptions, simplify Option to func type, rename GetOptConfig to ApplyOptions

--- glog/option.go
+++ glog/option.go
@@ -16,51 +16,43 @@ type FieldHookFunc func(fields []Field)
 
 type MessageHookFunc func(message string) string
 
-type Option interface {
-	apply(cfg *OptConfig)
-}
-
-type OptConfig struct {
+type LoggerOptions struct {
 	CallerSkip      int
 	FieldHookFunc   FieldHookFunc
 	MessageHookFunc MessageHookFunc
 	EnableOTELTrace *bool
 }
 
-type option func(cfg *OptConfig)
-
-func (fn option) apply(cfg *OptConfig) {
-	fn(cfg)
-}
+type Option func(*LoggerOptions)
 
 func WithCallerSkip(skip int) Option {
-	return option(func(cfg *OptConfig) {
+	return func(cfg *LoggerOptions) {
 		cfg.CallerSkip = skip
-	})
+	}
 }
 
 func WithFieldHookFunc(fn FieldHookFunc) Option {
-	return option(func(cfg *OptConfig) {
+	return func(cfg *LoggerOptions) {
 		cfg.FieldHookFunc = fn
-	})
+	}
 }
 
 func WithMessageHookFunc(fn MessageHookFunc) Option {
-	return option(func(cfg *OptConfig) {
+	return func(cfg *LoggerOptions) {
 		cfg.MessageHookFunc = fn
-	})
+	}
 }
 
 func WithOTELTrace(enabled bool) Option {
-	return option(func(cfg *OptConfig) {
+	return func(cfg *LoggerOptions) {
 		cfg.EnableOTELTrace = &enabled
-	})
+	}
 }
 
-func GetOptConfig(opts ...Option) *OptConfig {
-	cfg := &OptConfig{}
+func ApplyOptions(opts ...Option) *LoggerOptions {
+	cfg := &LoggerOptions{}
 	for _, opt := range opts {
-		opt.apply(cfg)
+		opt(cfg)
 	}
 	return cfg
 }
